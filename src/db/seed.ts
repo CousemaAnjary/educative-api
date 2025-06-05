8661929604139d810605c6531710aad7f6c790c8
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-05 16:06:19 +0300

    feat(db): :sparkles: ajouter un script de seed pour insérer des rôles dans la base de données

---
+++ src/db/seed.ts
@@ -0,0 +1,16 @@
+import db from "./drizzle";
+import { roles } from "./schema";
+
+async function seed() {
+  await db.insert(roles).values([
+    { name: "admin" },
+    { name: "enseignant" },
+    { name: "eleve" },
+  ]).onConflictDoNothing(); // évite l'erreur si déjà inséré
+
+  console.log("Rôles insérés avec succès !");
+}
+
+seed().catch((e) => {
+  console.error("Erreur lors du seed :", e);
+});
