3f9cd7264bdaebbb9cbb7307582b30bdd8db3b09
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-06 09:43:42 +0300

    feat(auth): :sparkles: ajouter la logique de connexion avec validation et génération de token JWT

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,12 @@
-import db from "../db/drizzle"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import { eq } from "drizzle-orm"
-import { roles, users } from "../db/schema"
 import { Request, Response } from "express"
-import { registerSchema } from "../schema/authSchema"
+import db from "../db/drizzle"
+import { roles, users } from "../db/schema"
+import { loginSchema, registerSchema } from "../schema/authSchema"
 
+const JWT_SECRET = process.env.JWT_SECRET as string 
 
 module.exports = {
   async register(req: Request, res: Response) {
@@ -44,5 +46,31 @@ module.exports = {
     }
   },
 
-  async login(req: Request, res: Response) {},
+  async login(req: Request, res: Response) {
+    try {
+       // Validation des données d'entrée (Zod)
+       const validatedData = loginSchema.safeParse(req.body)
+       if (!validatedData.success) return res.status(400).json({ success: false, error: validatedData.error.format() })
+
+        // Destructuration des données validées
+      const { email, password } = validatedData.data
+
+      // Recherche de l'utilisateur dans la base de données
+      const user = await db.query.users.findFirst({ where: eq(users.email, email) })
+      if (!user) return res.status(401).json({ success: false, message: "Invalid email" })
+
+      // Comparer les mot de passe
+      const isPasswordValid = await bcrypt.compare(password, user.password)
+      if (!isPasswordValid) return res.status(401).json({ success: false, message: "Invalid password" })
+
+      // Générer un token JWT (à implémenter)
+      const token = jwt.sign({ id: user.id, email: user.email , role:user.roleId }, JWT_SECRET, { expiresIn: "1h" })
+
+      // Retourner une réponse de succès avec le token
+      return res.status(200).json({success: true, message: "Login successful", token })
+
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error", success: false })
+    }
+  },
 }
--- src/schema/authSchema.ts
+++ src/schema/authSchema.ts
@@ -6,3 +6,8 @@ export const registerSchema = z.object({
   password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
   image : z.string().url({ message: "Invalid image URL" }).optional(),
 })
+
+export const loginSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }).min(1, { message: "Email is required" }),
+  password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+})
