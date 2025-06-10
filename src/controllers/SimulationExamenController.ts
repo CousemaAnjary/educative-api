44a0294c1517490579d2a3316253ec814a2e4d28
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-10 15:45:12 +0300

    feat(simulationExamen): implémenter le contrôleur et le service pour la gestion des simulations d'examen

--- src/controllers/SimulationExamenController.ts
+++ src/controllers/SimulationExamenController.ts
@@ -1,15 +1,92 @@
 import { Request, Response } from "express"
+import { simulationExamenSchema } from "../schema/simulationExamen"
+import { SimulationExamenService } from "../services/SimulationExamenService"
 
 module.exports = {
-  async getAllSimulationsExamen(req: Request, res: Response) {},
-
-  async createSimulationExamen(req: Request, res: Response) {
-    
+  async getAllSimulationsExamen(req: Request, res: Response) {
+    try {
+      // Récupération de toutes les simulations d'examen
+      const simulationsList = await SimulationExamenService.getAllSimulations();
+      return res.status(200).json({ success: true, simulations: simulationsList });
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ success: false, message: error.message });
+      }
+      // En cas d'erreur inattendue,
+      return res.status(500).json({ success: false, message: "Une erreur inconnue est survenue." });
+    }
   },
 
-  async getSimulationExamenById(req: Request, res: Response) {},
+  async createSimulationExamen(req: Request, res: Response) {
+    // Validation des données d'entrée (Zod)
+            const validatedData = simulationExamenSchema.safeParse(req.body)
+            if (!validatedData.success) return res.status(400).json({ success: false, error: validatedData.error.format() }) 
 
-  async updateSimulationExamen(req: Request, res: Response) {},
+    try {
+      await SimulationExamenService.createSimulation(validatedData.data);
+      return res.status(201).json({ success: true, message: "Simulation created successfully" });
 
-  async deleteSimulationExamen(req: Request, res: Response) {},
+    }
+    catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ success: false, message: error.message });
+      }
+      // En cas d'erreur inattendue,
+      return res.status(500).json({ success: false, message: "Une erreur inconnue est survenue." });
+    }
+  },
+
+  async getSimulationExamenById(req: Request, res: Response) {
+    const simulationId = req.params.id;
+
+    try {
+      const simulation = await SimulationExamenService.getSimulationById(simulationId);
+      if (!simulation) return res.status(404).json({ success: false, message: "Simulation not found" });
+
+      return res.status(200).json({ success: true, simulation });
+
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ success: false, message: error.message });
+      }
+      // En cas d'erreur inattendue,
+      return res.status(500).json({ success: false, message: "Une erreur inconnue est survenue." });
+    }
+  },
+
+  async updateSimulationExamen(req: Request, res: Response) {
+    // Validation des données d'entrée (Zod)
+    const validatedData = simulationExamenSchema.safeParse(req.body);
+    if (!validatedData.success) return res.status(400).json({ success: false, error: validatedData.error.format() });
+
+    const simulationId = req.params.id;
+
+    try {
+      await SimulationExamenService.updateSimulation(simulationId, validatedData.data);
+      return res.status(200).json({ success: true, message: "Simulation updated successfully" });
+
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ success: false, message: error.message });
+      }
+      // En cas d'erreur inattendue,
+      return res.status(500).json({ success: false, message: "Une erreur inconnue est survenue." });
+    }
+  },
+
+  async deleteSimulationExamen(req: Request, res: Response) {
+    const simulationId = req.params.id;
+
+    try {
+      await SimulationExamenService.deleteSimulation(simulationId);
+      return res.status(200).json({ success: true, message: "Simulation deleted successfully" });
+
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ success: false, message: error.message });
+      }
+      // En cas d'erreur inattendue,
+      return res.status(500).json({ success: false, message: "Une erreur inconnue est survenue." });
+    }
+  },
 }
--- src/schema/simulationExamen.ts
+++ src/schema/simulationExamen.ts
@@ -1,3 +1,28 @@
-import { z } from "zod"
+import { z } from "zod";
 
-
+export const simulationExamenSchema = z.object({
+  nom: z.string().min(1, { message: "Le nom est requis" }),
+  etat: z.enum(["Brouillon", "published", "archived"]).default("Brouillon"),
+  duree: z.number().int().min(1, { message: "La durée doit être un entier positif" }),
+  date: z.union([
+    z.date(),
+    z.string().transform((val) => new Date(val))
+  ]),
+  date_limite: z.union([
+    z.date(),
+    z.string().transform((val) => new Date(val))
+  ]),
+  questions: z.array(
+    z.object({
+      enoncer: z.string(),
+      options: z.array(
+        z.object({
+          label: z.string(),
+          value: z.string(),
+          correct: z.boolean()
+        })
+      )
+    })
+  ),
+  matiereId: z.number().int({ message: "ID de la matière requis" })
+});
